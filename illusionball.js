63b1eba4db82b8b61ad74121ed136c5d0853d139
BlackStar1991 <zirkaandry@ukr.net>
2017-01-17 19:17:58 +0200

    fixing mistakes 1

--- illusionball.js
+++ illusionball.js
@@ -4,11 +4,13 @@ function IllusionBall(canvaElement) {
 
     this.initialize = function () {
 
-        var canvas = document.getElementById(this.canvaElement),
-            n = 48,  // количество углов по умолчанию
-            corners = $('.corners'),
-            num = $('span'),
-            a = corners.val();
+        var canvas, numberOfCorn, corners, numberField, a, color;
+        color = ["#2E3691", "#485A8B", "#9B2A89", "#E6194D", "#F31C26", "#F27E1E", "#FFC80D", "#AFCA3E", "#15A14F", "#5B9E71", "#54C6D3", "#0CAAE3"];
+        canvas = document.getElementById(this.canvaElement);
+        numberOfCorn = 48;
+        corners = $(".corners");
+        numberField = $('span');
+        a = corners.val();
 
         $(window).resize(function () {
             var halfScreen = $(window).height();
@@ -16,15 +18,15 @@ function IllusionBall(canvaElement) {
             canvas.style.height = (halfScreen / 2 ) + "px";
         });
 
-        num.text(a);
+        numberField.text(a);
 
-        $('.corners').on("input", function () {
+        corners.on("input", function () {
             a = corners.val();
             console.log(a + "  =a");
-            num.text(a);
-            n = a;
+            numberField.text(a);
+            numberOfCorn = a;
             $this.createCircle();
-        })
+        });
 
         var ctx = canvas.getContext('2d');
 
@@ -35,17 +37,16 @@ function IllusionBall(canvaElement) {
             var radius = 10,
                 x = (canvas.width) / 2,
                 y = (canvas.height) / 2,
-                color = ["#2E3691", "#485A8B", "#9B2A89", "#E6194D", "#F31C26", "#F27E1E", "#FFC80D", "#AFCA3E", "#15A14F", "#5B9E71", "#54C6D3", "#0CAAE3"];
-            var c = 0;
+                c = 0;
             var step = 1;
 
-            for (var i = 0; i <= n; i++) {
+            for (var i = 0; i <= numberOfCorn; i++) {
                 var R = step * radius;
                 var f = Math.PI / 2;
                 if (c <= color.length) {  // задаем цвет
                     ctx.beginPath();
-                    var X = x + R * Math.cos(f + ((Math.PI * 2 * i) / n));
-                    var Y = y + R * Math.sin(f + ((Math.PI * 2 * i) / n));
+                    var X = x + R * Math.cos(f + ((Math.PI * 2 * i) / numberOfCorn));
+                    var Y = y + R * Math.sin(f + ((Math.PI * 2 * i) / numberOfCorn));
                     ctx.arc(X, Y, radius, 0, Math.PI * 2, true);
                     ctx.fillStyle = color[c];
                     ctx.fill();
@@ -54,16 +55,16 @@ function IllusionBall(canvaElement) {
                     if (c == color.length) {
                         c = 0;
                     }
-                    if ((i == n) && ( (radius * step)) < x * 0.85) {
+                    if ((i == numberOfCorn) && ( (radius * step)) < x * 0.85) {
                         step++;
                         radius = 1.1 * radius;
                         i = 0;
                     }
                 }
             }
-        }
+        };
 
         this.createCircle();
     }
-};
+}
 
